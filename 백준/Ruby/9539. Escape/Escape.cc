72d129da978f504770fbf12ca4208b38378db7f4
Lackoy <lackoy09@gmail.com>
2026-03-16 08:53:57 +0900

    [Ruby V] Title: Escape, Time: 1760 ms, Memory: 64328 KB -BaekjoonHub

--- 백준/Ruby/9539. Escape/Escape.cc
+++ 백준/Ruby/9539. Escape/Escape.cc
@@ -1,95 +1,134 @@
-#include<iostream>
+#include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
+#include <algorithm>
+
 using namespace std;
-
 typedef long long ll;
-typedef pair<ll, ll> pll;
 
-ll a[201010], b[201010];
-int par[201010], p[201010], goal;
-vector<int> e[201010];
+const int MAXn = 200005;
 
-void dfs(int now, int pre){
-	p[now] = pre;
-	for(auto nxt : e[now])
-        if(nxt != pre) dfs(nxt, now);
-}
-
-struct Vertex{
-	ll a; ll b;
-    int v; int sz;
+struct Segment {
+    ll min_req;
+    ll gain;
+    bool operator>(const Segment& other) const {
+        if (min_req != other.min_req) return min_req > other.min_req;
+        return gain < other.gain;
+    }
 };
 
-struct cmp{
-	bool operator()(const Vertex& A, const Vertex& B){
-		if(A.b-A.a >= 0 && B.b-B.a < 0) return 0;
-		if(A.b-A.a < 0 && B.b-B.a >= 0) return 1;
-		if(A.b-A.a >= 0) return A.a >= B.a;
-		return A.b <= B.b;
-	}
-};
+vector<int> adj[MAXn];
+ll H[MAXn];
+bool is_on_path[MAXn];
+priority_queue<Segment, vector<Segment>, greater<Segment>> pq[MAXn];
 
-priority_queue<Vertex, vector<Vertex>, cmp> pq;
+void dfs_sub(int u, int p) {
+    for (int v : adj[u]) {
+        if (v == p || is_on_path[v]) continue;
+        dfs_sub(v, u);
+        
+        if (pq[u].size() < pq[v].size()) swap(pq[u], pq[v]);
+        while (!pq[v].empty()) {
+            pq[u].push(pq[v].top());
+            pq[v].pop();
+        }
+    }
 
-int f(int x){
-	return par[x] < 0 ? x : par[x] = f(par[x]);
+    ll cur_req = (H[u] < 0) ? -H[u] : 0;
+    ll cur_gain = H[u];
+
+    while (!pq[u].empty()) {
+        Segment top = pq[u].top();
+        if (cur_gain <= 0 || top.min_req <= cur_req) {
+            pq[u].pop();
+            cur_req = max(cur_req, top.min_req - cur_gain);
+            cur_gain += top.gain;
+        } else break;
+    }
+
+    if (cur_gain > 0) pq[u].push({cur_req, cur_gain});
+    else while (!pq[u].empty()) pq[u].pop();
 }
 
-void Union(int v){
-	v = f(v);
-    p[v] = f(p[v]);
-	ll aa = max(a[p[v]], a[p[v]]-b[p[v]]+a[v]);
-	ll bb = b[p[v]]+b[v]-a[p[v]]-a[v]+aa;
-	
-	if(b[p[v]]-a[p[v]] >= bb-aa) return;
-	
-	a[p[v]] = aa;
-	b[p[v]] = bb;
-	par[p[v]] += par[v];
-	par[v] = p[v];
-	if(p[v] != 1) pq.push({a[p[v]], b[p[v]], p[v], par[p[v]]});
+void solve() {
+    int n, t;
+    if (!(cin >> n >> t)) return;
+
+    for (int i = 1; i <= n; i++) {
+        adj[i].clear();
+        while (!pq[i].empty()) pq[i].pop();
+        is_on_path[i] = false;
+    }
+
+    for (int i = 1; i <= n; i++) cin >> H[i];
+
+    for (int i = 0; i < n - 1; i++) {
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    vector<int> parent(n + 1, 0);
+    queue<int> q;
+    q.push(1);
+    parent[1] = -1;
+    
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        if (u == t) break;
+        for (int v : adj[u]) {
+            if (parent[v] == 0) {
+                parent[v] = u;
+                q.push(v);
+            }
+        }
+    }
+
+    vector<int> path;
+    int curr = t;
+    while (curr != -1) {
+        path.push_back(curr);
+        is_on_path[curr] = true;
+        curr = parent[curr];
+    }
+    reverse(path.begin(), path.end());
+
+    ll current_hp = 0;
+    priority_queue<Segment, vector<Segment>, greater<Segment>> total_pq;
+
+    for (int node : path) {
+        current_hp += H[node];
+        if (current_hp < 0) {
+            cout << "trapped\n";
+            return;
+        }
+
+        for (int v : adj[node]) {
+            if (is_on_path[v]) continue;
+            dfs_sub(v, node);
+            while (!pq[v].empty()) {
+                total_pq.push(pq[v].top());
+                pq[v].pop();
+            }
+        }
+
+        while (!total_pq.empty() && current_hp >= total_pq.top().min_req) {
+            current_hp += total_pq.top().gain;
+            total_pq.pop();
+        }
+    }
+
+    cout << "escaped\n";
 }
 
-int main(){
-    ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-
-	int T;
-    cin >> T;
-    while(T--){
-		int n;
-        cin >> n >> goal;
-
-		for(int i = 1; i <= n; i++){
-			par[i] = -1;
-			a[i] = b[i] = 0;
-			e[i].clear();
-		}
-
-		for(int i = 1; i <= n; i++){
-			int x; cin >> x;
-			if(x > 0) b[i] = x;
-			else a[i] = -x;
-			if(i == goal) b[i] += 1234123412341234LL;
-			if(i != 1) pq.push({a[i], b[i], i, -1});
-		}
-
-		for(int i = 1; i < n; i++){
-			int u, v;
-            cin >> u >> v;
-			e[u].push_back(v);
-			e[v].push_back(u);
-		}
-
-		dfs(1, 1);
-		while(pq.size()){
-			auto now = pq.top(); pq.pop();
-			if(now.sz != par[now.v]) continue;
-			Union(now.v);
-		}
-
-		cout << (a[1] > 0 ? "trapped\n" : "escaped\n");
-	}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int tc;
+    if (cin >> tc) {
+        while (tc--) solve();
+    }
+    return 0;
 }
