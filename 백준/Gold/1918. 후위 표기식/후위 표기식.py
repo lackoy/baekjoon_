53e2dab79b634cf96c880b011ff8606cfd85f7eb
Lackoy <lackoy09@gmail.com>
2026-04-09 08:04:07 +0900

    [Gold II] Title: 후위 표기식, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -0,0 +1,23 @@
+array = input()
+stack = []
+result = ''
+for i in array:
+    if i == '(':
+        stack.append(i)
+    elif i == ')':
+        while stack and stack[-1] != '(':
+            result += stack.pop()
+        stack.pop()
+    elif i == '*' or i == '/':
+        while stack and (stack[-1] == '*' or stack[-1] == '/'):
+            result += stack.pop()
+        stack.append(i)
+    elif i == '+' or i == '-':
+        while stack and stack[-1] != '(':
+            result += stack.pop()
+        stack.append(i)
+    else:
+        result += i
+while stack:
+    result += stack.pop()
+print(result)
